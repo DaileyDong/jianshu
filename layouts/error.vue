4f060d0ae7283c48752b3ad79d926506be40ebec
DaileyDong <13203878630@163.com>
2018-01-24 18:23:35 +0800

    完成404页

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,36 +1,71 @@
 <template>
-  <section class="container">
-    <h1 class="title">
-      {{ error.statusCode }}
-    </h1>
-    <h2 class="info">
-      {{ error.message }}
-    </h2>
-    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      返回首页
+<div class="error">
+    <nuxt-link class="logo" to="/">
+    <img src="../assets/img/404-logo.png" alt="logo">
     </nuxt-link>
-  </section>
+     <div class="container">
+          <div class="photo">
+      <img src="../assets/img/404.gif" alt="404">
+    </div>
+     <div class="msg">
+           <h4>您要找的页面不存在！</h4>
+    <h6>可能是因为您的链接地址有误、该文章已经被作者删除或转为私密状态。</h6>
+    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
+         返回 [简书] 首页
+    </nuxt-link>
+     </div>
+     </div>
+</div>
+
+
 </template>
 <script>
 export default {
-  props: ['error']
-}
+  props: ["error"]
+};
 </script>
 
 <style scoped>
-.title
-{
-  margin-top: 15px;
-  font-size: 5em;
+
+.logo img {
+  position: relative;
+  top: 50px;
+  margin-left: 50px;
+  width: 300px;
 }
-.info
-{
-  font-weight: 300;
-  color: #9aabb1;
-  margin: 0;
+.container{
+  position: relative;
+    top: 20px;
+  text-align: center;
 }
-.button
-{
-  margin-top: 50px;
+.photo {
+  margin: 50px auto 0;
+  width:700px;
+  box-sizing: border-box;
+  text-align: center;
+}
+.photo img{
+  width:100%;
+}
+h6 {
+  margin: 30px;
+  font-size: 14px;
+}
+.button {
+  margin: 5px auto;
+  border-radius: 40px;
+  font-size: 15px;
+  color: #fff !important;
+  width: 200px;
+  padding: 13px;
+  background-color: #42c02e;
+}
+@media (max-width:768px) {
+  .logo{
+    display: none;
+  }
+  .photo{
+      width:330px;
+  }
 }
 </style>
