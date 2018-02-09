8c0ccaa28531d147dbefbfab5df94e24f5722e81
dongbangjun <13203878630.@163.com>
2018-02-09 11:33:32 +0800

    完成消息键的内页布局，完善之前代码

---
+++ pages/notifications/comments.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <my-header></my-header>
+    <div class="notification my-container">
+      <div class="row">
+        <div class="aside col-4">
+          <my-aside></my-aside>
+        </div>
+        <div class="main col-8"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import MyHeader from "~/components/MyHeader";
+import MyAside from '~/components/MyAside';
+export default {
+  name: "comments",
+  components: {
+    MyHeader,
+    MyAside
+  }
+};
+</script>
+<style scoped>
+.notification .aside{
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  margin: 86px 0 0;
+  padding: 0 0 30px;
+  width: 280px;
+  overflow: auto;
+}
+</style>
+
---
+++ pages/subscriptions/timeline.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <my-header></my-header>
+      关注
+  </div>
+</template>
+<script>
+import MyHeader from "~/components/MyHeader";
+export default {
+  name:'timeline',
+   components:{
+    MyHeader
+  }
+}
+</script>
+<style scoped>
+
+</style>
