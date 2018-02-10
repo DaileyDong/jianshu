4010b5a2086789b7e2d8d6a81aa901de8fc20844
dongbangjun <13203878630.@163.com>
2018-02-10 13:14:44 +0800

    消息键的简信和评论

--- pages/notifications/chats.vue
+++ pages/notifications/chats.vue
@@ -3,10 +3,46 @@
     <my-header></my-header>
     <div class="notification my-container">
       <div class="row">
-        <div class="aside col-4">
+        <div class="aside col-4" >
           <my-aside></my-aside>
         </div>
-        <div class="main col-8"></div>
+        <div class="main col-8" >
+          <div class="menu">全部简书</div>
+          <ul class="jianxin-list ">
+            <li class="clearfix">
+              <div class="pull-right">
+                <span class="time">02.05 11:20</span>
+                 <i class="fa fa-angle-down"></i>
+              </div>
+              <nuxt-link to="/u/123" class="avatar">
+              <img src="../../static/89731766ca20.jpg" alt="">
+              </nuxt-link>
+               <div class="info">
+                <nuxt-link to="/u/123" class="name">
+                 薛文诚
+              </nuxt-link>
+              <a href="javascript:void(0)" class="wrap">
+                <p>你现在在干嘛？</p>
+              </a>
+               </div>
+            </li>
+                <li>
+              <div class="pull-right" >
+                <span class="time">02.05 11:20</span>
+                 <i class="fa fa-angle-down"></i>
+              </div>
+              <nuxt-link to="/u/123" class="avatar">
+              <img src="../../static/89731766ca20.jpg" alt="">
+              </nuxt-link>
+                <nuxt-link to="/u/123" class="name">
+                 薛文诚
+              </nuxt-link>
+              <a href="javascript:void(0)" class="wrap">
+                <p>你现在在干嘛fgsadgfsdgsdgsdf功夫大使馆第三方dsafasdfasdfsaafasd公司的公司大股东是大哥大法官的轨道上 收水电费感受到法国大使馆</p>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -23,13 +59,60 @@ export default {
 };
 </script>
 <style scoped>
-.notification .aside{
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  margin: 86px 0 0;
+.notification .aside {
+  margin-top: 30px;
   padding: 0 0 30px;
-  width: 280px;
+  max-width: 280px;
   overflow: auto;
 }
+.notification .main {
+  margin-top: 30px;
+  padding-left: 30px;
+}
+.notification .main .menu {
+  margin-bottom: 20px;
+  font-size: 14px;
+  font-weight: 700;
+}
+.notification .main .jianxin-list li{
+position: relative;
+border-top: 1px solid #f0f0f0;
+}
+.notification .main .jianxin-list .pull-right{
+  margin: 20px 20px 0 0;
+  font-size: 13px;
+}
+.notification .main .jianxin-list .pull-right .time{
+  color:#9b9b9b;
+}
+.notification .main .jianxin-list .pull-right i{
+  color: #9b9b9b;
+  margin-left: 5px;
+  cursor: pointer;
+}
+.notification .main .jianxin-list .avatar{
+ float: left;
+ width: 48px;
+ height: 48px;
+ margin: 20px 10px 20px 20px;
+}
+.notification .main .jianxin-list .name{
+  position:absolute;
+  top: 25px;
+  font-size: 15px;
+}
+.notification .main .jianxin-list .wrap{
+  display:block;
+  padding: 20px 20px 20px 0;
+  min-height:80px;
+}
+.notification .main .jianxin-list p{
+  margin-top:28px;
+  font-size: 12px;
+  color: #999;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space:nowrap;
+}
+
 </style>
--- pages/notifications/comments.vue
+++ pages/notifications/comments.vue
@@ -3,34 +3,170 @@
     <my-header></my-header>
     <div class="notification my-container">
       <div class="row">
-        <div class="aside col-4">
+        <div class="aside col-4" >
           <my-aside></my-aside>
         </div>
-        <div class="main col-8"></div>
+        <div class="main col-8">
+          <div class="menu">收到的评论</div>
+          <ul class="comment-list">
+            <li v-for="(notification,index) in notifications" :key="index">
+              <nuxt-link to="/u/123" class="avatar">
+              <img :src="notification.source.user.avatar_source" alt="">
+              </nuxt-link>
+              <div class="info">
+                  <nuxt-link to="/u/123" class="user">{{notification.source.user.nickname}}</nuxt-link>
+                  <span class="comment-slogan">在文章</span>
+                  <nuxt-link to="/p/123">《{{notification.source.note.title}}》</nuxt-link>
+                   <span class="comment-slogan">的评论中提到了你</span>
+                <div class="time">{{notification.created_at | formateDate}}</div>
+              </div>
+              <p v-html="notification.source.compiled_content">
+              </p>
+              <div class="meta">
+                <a href="javascript:void(0)" class="function-btn">
+                  <i class="fa fa-comment-o"></i>
+                  <span>回复</span>
+                </a>
+                 <nuxt-link to="/p/123" class="function-btn">
+                 <i class="fa fa-share"></i>
+                 <span>查看对话</span>
+                 </nuxt-link>
+              </div>
+             
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 import MyHeader from "~/components/MyHeader";
-import MyAside from '~/components/MyAside';
+import MyAside from "~/components/MyAside";
 export default {
   name: "comments",
+  data() {
+    return {
+      activeIndex: [],
+      emojiIndex: [],
+      notifications:[
+        {
+          id:230306867,
+          is_read:true,
+          created_at:"2018-02-05T15:32:01.000+08:00",
+          source:{
+            id:20332501,
+            user_id:3106924,
+            parent_id:20152283,
+            compiled_content:'<a href="/users/b673ad6d3088" class="maleskine-author" target="_blank" data-user-slug="b673ad6d3088">@朱庇特的白银时代</a> 谢谢',
+            note:{
+              id:23397978,
+              title:"冬季里看花",
+            },
+            user:{
+              id:3106924,
+              nickname:"月儿上山了",
+              avatar_source:"/1d2d4fe929e5.jpg",
+            },
+          },
+        },
+
+         {
+          id:230306867,
+          is_read:true,
+          created_at:"2018-02-07T10:09:01.000+08:00",
+          source:{
+            id:20332501,
+            user_id:3106924,
+            parent_id:20152283,
+            compiled_content:'<a href="/users/b673ad6d3088" class="maleskine-author" target="_blank" data-user-slug="b673ad6d3088">@朱庇特的白银时代</a> 非常赞同',
+            note:{
+              id:23397978,
+              title:"神秘巨星",
+            },
+            user:{
+              id:3106924,
+              nickname:"薛文诚",
+              avatar_source:"/89731766ca20.jpg",
+            },
+          },
+        },
+      ]
+    };
+  },
   components: {
     MyHeader,
     MyAside
   }
 };
 </script>
-<style scoped>
-.notification .aside{
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  margin: 86px 0 0;
+<style>
+.notification .aside {
+  margin-top: 30px;
   padding: 0 0 30px;
-  width: 280px;
+  max-width: 280px;
   overflow: auto;
+}
+.notification .main {
+  margin-top: 30px;
+  padding-left: 30px;
+}
+.notification .main .menu {
+  margin-bottom: 20px;
+  font-size: 14px;
+  font-weight: 700;
+}
+.notification .main .comment-list li {
+  padding: 20px;
+  border-top: 1px solid #f0f0f0;
+}
+.notification .main .comment-list .avatar {
+  width: 40px;
+  height: 40px;
+  display: inline-block;
+}
+.notification .main .comment-list .info {
+  display: inline-block;
+  font-size: 15px;
+  margin-left: 15px;
+  vertical-align: middle;
+}
+.notification .main .comment-list .info .user {
+  margin-right: 5px;
+  color: #333 !important;
+}
+.notification .main .comment-list .info a {
+  color: #3194d0 !important;
+}
+.notification .main .comment-list .info .time {
+  margin-top: 2px;
+  font-size: 12px;
+  color: #969696;
+}
+.notification .main .comment-list p {
+  margin-top: 10px;
+  font-size: 15px;
+}
+.notification .main .comment-list p .maleskine-author {
+  color: #3194d0 !important;
+}
+.notification .main .comment-list .meta {
+  margin-top: 10px;
+  height: 20px;
+}
+.notification .main .comment-list .meta .function-btn {
+  float: left;
+  margin-right: 30px;
+  font-size: 13px;
+  color: #969696 !important;
+}
+.notification .main .comment-list .meta .function-btn i {
+  margin-right: 5px;
+  font-size: 15px;
+  vertical-align: middle;
+}
+.notification .main .comment-list .meta .function-btn span{
+  vertical-align: middle;
 }
 </style>
 
